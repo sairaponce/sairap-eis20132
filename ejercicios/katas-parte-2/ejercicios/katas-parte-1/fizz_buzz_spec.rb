b2e1b125bb955a6098034e3cd0742d5191aa4c3a
sairaponce <saira.ponce.ici@gmail.com>
2013-09-23 05:39:31 +0200

    Borrando archivos

--- ejercicios/katas-parte-2/ejercicios/katas-parte-1/fizz_buzz_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rspec'
-require_relative './fizz_buzz.rb'
-
-describe 'analizarFB' do
-	it 'Si se analiza el 6 debe imprimir Fizz' do 
-		fizzbuzz = FizzBuzz.new 
-		fizzbuzz.analizarFB(6).should eq "Fizz"
-	end
-end
